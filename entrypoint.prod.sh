fdf673f3f1ffd5c9d967b686e022ec2b7eec3a71
Kyle Salling <ksalling@gmail.com>
2026-01-01 02:52:02 -0800

    entrypoint chnages

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python3 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput --settings=django_project.settings.prod
 python3 manage.py migrate --noinput
 python3 -m gunicorn --bind 0.0.0.0:8282 --workers 3 django_project.wsgi:application
