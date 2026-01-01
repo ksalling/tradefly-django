969cbee38efab78ecda320665c18823cd34f9ee2
Kyle Salling <ksalling@gmail.com>
2025-12-31 19:09:04 -0800

    secret key

--- django_project/settings/prod.py
+++ django_project/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ${{SECRET_KEY}}
+SECRET_KEY = django-insecure-w#8yes#)v!3j3%-_5lzq#)ovvaw$wy8o%+orb0d68n!41b%7kb
 
 ALLOWED_HOSTS = ${{DJANGO_ALLOWED_HOSTS}}
 CSRF_TRUSTED_ORIGINS = ${{DJANGO_CSRF_TRUSTED_ORIGINS}}
