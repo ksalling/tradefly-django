52508455cfced38f0b31918dfee82925d9ece0ca
Kyle Salling <ksalling@gmail.com>
2026-01-02 10:34:07 -0800

    changed entrypoint

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 python3 manage.py collectstatic --noinput --settings=django_project.settings.prod
-python3 manage.py makemigrations --noinput --settings=django_project.settings.prod
 python3 manage.py migrate --noinput --settings=django_project.settings.prod
 python3 -m gunicorn --bind 0.0.0.0:8282 --workers 3 django_project.wsgi:application
